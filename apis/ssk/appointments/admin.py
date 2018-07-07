37925c41f8dab6845a6b4fadb5ea7a501e377546
Anirudh Konduru <anirudhmkonduru@gmail.com>
2018-07-07 17:16:28 +0530

    added models

--- apis/ssk/appointments/admin.py
+++ apis/ssk/appointments/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Patient, Medi, MediAvailability, Appointment, Reception, SocialWorker
+
 
 # Register your models here.
+admin.site.register(Patient)
+admin.site.register(Medi)
+admin.site.register(MediAvailability)
+admin.site.register(Appointment)
+admin.site.register(Reception)
+admin.site.register(SocialWorker)
--- apis/ssk/appointments/models.py
+++ apis/ssk/appointments/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
+
 # Create your models here.
+class Patient(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.PositiveIntegerField()
+    phone = models.CharField(max_length=10)
+    address = models.CharField(max_length=500)
+    disorder = models.CharField(max_length=500)
+    comment = models.CharField(max_length=500)
+
+
+class Medi(models.Model):
+    name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=10)
+    spec = models.CharField(max_length=500)
+
+
+class MediAvailability(models.Model):
+    medi = models.ForeignKey(Medi, on_delete=models.CASCADE)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+
+
+class Appointment(models.Model):
+    medi = models.ForeignKey(Medi, on_delete=models.CASCADE)
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+
+    confirmed = models.BooleanField(default=False)
+    cancelled = models.BooleanField(default=False)
+
+    comments = models.CharField(max_length=500)
+
+
+class Reception(models.Model):
+    name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=10)
+
+
+class SocialWorker(models.Model):
+    name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=10)
